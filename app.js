c67b4d2102fbfc14a0a1bffbffd5f875a6e6d945
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-13 15:27:59 +0300

    get; fetching books from the database

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const app = express();
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 
-const app = express();
-
-app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+main().catch(err => console.log(err));
+
+async function main() {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect('mongodb://127.0.0.1:27017/booksDB');
+}
+
+// ////////////////////////////////////
+
+const bookSchema = {
+    title: String,
+    author: String,
+    content: String
+}
+
+const Book = mongoose.model("Book", bookSchema);
 
 
-
-
+app.get("/books", function(req, res) {
+    Book.find({})
+        .then((foundBooks) => {
+            res.send(foundBooks);
+        })
+})
 
 
 
